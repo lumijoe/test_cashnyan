2d59f56c38eea5e3261333a6c85c3bc6cdd3a39d
lumijoe <spacedesign63@gmail.com>
2024-01-05 12:19:12 +0900

    test

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,42 @@ import Head from 'next/head';
 import MessageBoard from '../components/MessageBoard';
 import Header from '../components/Header.js'
 
+
 export default function Index() {
 // const Index = () => {
   const [id, setId] = useState('');
+
+  const [idInput, setIdInput] = useState('');
+
+  const handleSetId = () => {
+    localStorage.setItem('id', idInput);
+    window.location.href = '/';
+  };
+
+  const handleButtonClick = (choice) => {
+    // ボタンで選択された値をidInputに設定
+    setIdInput(choice);
+  }
+
+  const handleRadioChange = (e) => {
+    // ボタンで選択された値をidInputに設定
+    setIdInput(e.target.value);
+  }
+
+  
+
+
+  useEffect(() => {
+    const storedId = localStorage.getItem('id');
+    if (storedId) {
+      setId(storedId);
+    }
+  }, []);
+
+  
+
+
+
 
   useEffect(() => {
     const storedId = localStorage.getItem('id');
@@ -27,6 +60,64 @@ export default function Index() {
         <title>Test CashNyan</title>
       </Head>
       <Header />
+
+
+
+  
+
+      <div role="main">
+        <h1 className='text-2xl'>支出カテゴリ入力</h1>
+        <div className="form-group">
+          <label htmlFor="id_input">何に使った？</label>
+          <div className='flex'>
+            <input
+              type="text"
+              id="id_input"
+              className="form-control bg-slate-200 border rounded w-[230px] text-sm h-[30px]"
+              onChange={(e) => setIdInput(e.target.value)}
+              placeholder="その他は入力してください"
+            />
+          </div>
+          <div>
+            
+          
+          <input
+          type="radio"
+          id="水"
+          name="options"
+          value="水"
+          checked={idInput === "水"}
+          onChange={handleRadioChange}
+        />
+        <label htmlFor="水">水</label>
+
+        <input
+          type="radio"
+          id="お茶"
+          name="options"
+          value="お茶"
+          checked={idInput === "お茶"}
+          onChange={handleRadioChange}
+        />
+        <label htmlFor="お茶">お茶</label>
+
+        <input
+          type="radio"
+          id="オヤツ"
+          name="options"
+          value="オヤツ"
+          checked={idInput === "オヤツ"}
+          onChange={handleRadioChange}
+        />
+        <label htmlFor="オヤツ">オヤツ</label>
+        </div>
+        </div>
+
+        <button onClick={handleSetId} className="bg-red-400 w-[280px] rounded-full">
+          カテゴリを決定
+        </button>
+      </div>
+
       
 
       <div role="main">
--- pages/login.js
+++ pages/login.js
@@ -2,6 +2,7 @@
 import { useState } from 'react';
 import Head from 'next/head';
 import Header from 'components/Header'
+import Link from 'next/link'
 
 
 import { useEffect } from 'react';
